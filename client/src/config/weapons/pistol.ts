3bb2b2fea15271f15e18e304a18e357a142e61d4
Ivan Bogomolov <hello@ivanbogomolov.ru>
2026-04-05 10:06:26 +0300

    docker compose

--- client/src/config/weapons/pistol.ts
+++ client/src/config/weapons/pistol.ts
@@ -1,7 +1,7 @@
 import { createUniformWeaponPlacement, type WeaponModelConfig } from './types'
 
 export const pistolModelConfig: WeaponModelConfig = {
-  id: 'colt_m4a1_low-poly',
+  id: 'm16',
   placementByLocomotion: createUniformWeaponPlacement({
     position: { x: 0.12, y: 0.02, z: -0.02 },
     rotation: { x: Math.PI / 2, y: -Math.PI / 2, z: 0 },
